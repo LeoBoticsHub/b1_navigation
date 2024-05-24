b2c6c9d1c907b907199c0a2997a0d3c6d06e9900
Federico Rollo <rollo.f96@gmail.com>
2024-05-24 08:14:33 +0200

    update params

--- launch/localization.launch.py
+++ launch/localization.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
         parameters=[{
             'target_frame': LaunchConfiguration('lidar_frame'),
             'transform_tolerance': 0.01,
-            'min_height': -0.7,
+            'min_height': -0.65,
             'max_height': 0.3,
             'angle_min': -3.14, # M_PI
             'angle_max': 3.14, # M_PI
